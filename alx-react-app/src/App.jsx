87d2c9f3d2102842341300a0712cd2ca44abd3af
Precious Fiberesima <fiberesimapreciousubong@gmail.com>
2025-08-31 13:56:14 +0100

    feat: add WelcomeMessage component with JSX updates

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,8 @@
+import WelcomeMessage from './components/WelcomeMessage';
 export default function App() {
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
-      <h1>Hello ALX 🚀</h1>
-      <p>My first React app is running successfully!</p>
+      <WelcomeMessage />
     </div>
   );
 }
