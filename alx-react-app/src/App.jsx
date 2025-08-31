5f0f0531d554ebcc6456ff59872dfa9c45d23438
Precious Fiberesima <fiberesimapreciousubong@gmail.com>
2025-08-31 14:16:35 +0100

    feat: create Header, MainContent, Footer components and use them in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,13 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 export default function App() {
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
