a7b9379950c251209be84cbb7e4b6bc961f65590
Precious Fiberesima <fiberesimapreciousubong@gmail.com>
2025-09-08 15:59:22 +0100

    Add Counter component with increment, decrement, and reset functionality

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 export default function App() {
   return (
@@ -13,7 +14,9 @@ export default function App() {
         age={25}
         bio="Loves hiking and photography"
       />
+      <Counter />
       <Footer />
     </div>
   );
 }
+
