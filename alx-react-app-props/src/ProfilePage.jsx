b8e762ee993054a823d31931f3d078e84524c9a3
Precious Fiberesima <fiberesimapreciousubong@gmail.com>
2025-09-09 09:47:37 +0100

    Delete alx-react-app-props directory

--- alx-react-app-props/src/ProfilePage.jsx
+++
@@ -1,7 +0,0 @@
-import UserProfile from './UserProfile';
-
-function ProfilePage() {
-  return <UserProfile />;
-}
-
-export default ProfilePage;
--- alx-react-app-props/src/UserContext.js
+++
@@ -1,9 +0,0 @@
-// src/UserContext.js
-
-import { createContext } from 'react';
-
-const UserContext = createContext(null);
-
-export default UserContext;
-
-
