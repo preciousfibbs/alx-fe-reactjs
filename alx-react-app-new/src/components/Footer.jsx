2e06e9ac79b90ff93fc0eaa42e9dc9b767b13111
Precious Fiberesima <fiberesimapreciousubong@gmail.com>
2025-09-09 11:47:47 +0100

    Add refactored project alx-react-app-props

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '0.5rem', marginTop: '1rem' }}>
-            <p>© 2023 City Lovers</p>
-        </footer>
-    );
-}
-
-export default Footer;
--- alx-react-app-new/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '1rem' }}>
-            <h1>My Favorite Cities</h1>
-        </header>
-    );
-}
-
-export default Header;
