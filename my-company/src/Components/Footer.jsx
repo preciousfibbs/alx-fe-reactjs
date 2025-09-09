e3140e312536ae4f8da3ed168722357b3a241cb0
Precious Fiberesima <fiberesimapreciousubong@gmail.com>
2025-09-09 13:32:10 +0100

    Delete my-company directory

--- my-company/src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ padding: '10px', backgroundColor: '#333', color: '#fff', textAlign: 'center' }}>
-      &copy; 2025 My Company. All rights reserved.
-    </footer>
-  );
-}
-
-export default Footer;
