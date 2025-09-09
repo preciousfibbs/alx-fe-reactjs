73dd58a4ecae539346d983e4f9a058679d0b37d4
Precious Fiberesima <fiberesimapreciousubong@gmail.com>
2025-09-09 13:11:22 +0100

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Services from './Pages/Services';
-import Contact from './Pages/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'; // optional
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -15,8 +16,10 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
 
 export default App;
+
